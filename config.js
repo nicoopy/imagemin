6e807fa9d107c4d94c94e3838af0c1bb32128c87
nicoopy <nicoopy@163.com>
2021-01-23 03:08:38 +0800

    update

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
   // target image directory
   buildPath: './image-min',
   // image quality between 0 and 100
-  imageQuality: 56,
+  imageQuality: 50,
   // open lossless compression
   progressive: true,
 };
